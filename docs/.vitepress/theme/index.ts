82747f53a19ae174c575e89b6767bb463c3a9f82
Liet Blue <lietblue@posteo.net>
2025-07-31 10:13:42 +0800

    Feat: enhanced readability

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,27 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import type { Theme } from "vitepress";
+import DefaultTheme from "vitepress/theme";
+import "./style.css";
+import {
+  NolebaseEnhancedReadabilitiesMenu,
+  NolebaseEnhancedReadabilitiesScreenMenu,
+} from "@nolebase/vitepress-plugin-enhanced-readabilities/client";
+
+import "@nolebase/vitepress-plugin-enhanced-readabilities/client/style.css";
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
+      "nav-bar-content-after": () => h(NolebaseEnhancedReadabilitiesMenu),
+      // A enhanced readabilities menu for narrower screens (usually smaller than iPad Mini)
+      "nav-screen-content-after": () =>
+        h(NolebaseEnhancedReadabilitiesScreenMenu),
+    });
   },
   enhanceApp({ app, router, siteData }) {
     // ...
-  }
-} satisfies Theme
+  },
+} satisfies Theme;
